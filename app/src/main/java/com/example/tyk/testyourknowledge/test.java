377b08e0cf14d682e072b5e502ab09b7976c85e3
shegd <shegun.mensah-beauclair@efrei.net>
2017-02-08 16:21:06 +0100

    test

--- app/src/main/java/com/example/tyk/testyourknowledge/test.java
+++ app/src/main/java/com/example/tyk/testyourknowledge/test.java
@@ -3,9 +3,12 @@ package com.example.tyk.testyourknowledge;
 
 public class test {
     private  String sheg;
+    private  String sheg2;
 
-    public test(String sheg) {
+    public test(String sheg, String shegg) {
          this.sheg = sheg;
+         this.sheg2 = shegg;
+
 
     }
 }
