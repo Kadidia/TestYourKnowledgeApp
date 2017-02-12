7229bc53b28a2c1dbaf8a9ce6261e25c0ac74876
Shegun <shegun.mensah-beauclair@efrei.net>
2017-02-12 22:52:37 +0100

    creation class courses et activity_courses

---
+++ app/src/main/java/com/example/tyk/testyourknowledge/CoursePage.java
@@ -0,0 +1,16 @@
+package com.example.tyk.testyourknowledge;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by shegun on 12/02/2017.
+ */
+
+public class CoursePage extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_course);
+    }
+}
