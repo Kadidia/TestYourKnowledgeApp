7b4290f427c87273df199dbb1aefd8703eab809b
Kadidiatou MARIKO <kadidiatou.mariko7@gmail.com>
2017-02-16 01:39:54 +0100

    Récuperation des modules dans la bdd et mise à jour du nom de l'utilisateur dans le menu

--- app/src/main/java/com/example/tyk/testyourknowledge/quiz_eleve.java
+++ app/src/main/java/com/example/tyk/testyourknowledge/quiz_eleve.java
@@ -19,7 +19,7 @@ import android.view.ViewGroup;
 
 import android.widget.TextView;
 
-public class quiz_eleve extends menu {
+public class quiz_eleve extends menu_principal{
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
