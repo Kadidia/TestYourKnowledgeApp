af93df7980eb463579d598dc1db88b6de8ad07dd
Kadi <kadidiatou.mariko7@gmail.com>
2017-02-16 05:49:38 +0100

    version améliorée (voir details sur w)

---
+++ app/src/main/java/com/example/tyk/testyourknowledge/eMarketing_quiz_fragment.java
@@ -0,0 +1,22 @@
+package com.example.tyk.testyourknowledge;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Axa on 16/02/2017.
+ */
+
+public class eMarketing_quiz_fragment extends android.support.v4.app.Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.emarketing_quiz_fragment, container, false);
+
+        return rootView;
+    }
+}
